b79961df0dffbeb1384e2ce32eac15da819cdd83
mleaf <mariesleaf@gmail.com>
2015-11-28 11:37:01 -0500

    add _copySessionToForm

--- P4_conference/FINAL_conference.py
+++ P4_conference/FINAL_conference.py
@@ -4,26 +4,150 @@
 
 Add Endpoints methods
 
-def getConferenceSessions(websafeConferenceKey):
- """Given a conference, return all sessions"""
- return allsessions
+def _copySessionToForm(self, sesh, websafeConferenceKey): # TODO 1 is it suppose to take a websafekey arg?
+    """Copy relevant fields from Session to SessionForm."""
+    sf = SessionForm()
+    for field in sf.all_fields():
+        if hasattr(sesh, field.name):
+            # convert Date to date string; just copy others
+            if field.name.endswith('Date'):
+                setattr(sf, field.name, str(getattr(sesh, field.name)))
+            else:
+                setattr(sf, field.name, getattr(sesh, field.name))
+        elif field.name == "websafeConferenceKey":
+            setattr(sf, field.name, conf.key.urlsafe())
+    if websafeConferenceKey:
+        setattr(sf, 'conference', websafeConferenceKey)
+    sf.check_initialized()
+    return cf
 
+def _createSession(self, request):
+""" -- open only to the organizer of the conference"""
+    # preload necessary data items
+    # ensure user = organizer of the conference
+    user = endpoints.get_current_user()
+    if not user:
+        raise endpoints.UnauthorizedException('Authorization required')
+    user_id = getUserId(user)
+
+    if not request.name:
+        raise endpoints.BadRequestException("Conference 'name' field required")
+
+    # fetch existing conference
+    conf = ndb.Key(urlsafe=request.websafeConferenceKey).get()
+
+    # copy SessionForm/ProtoRPC Message into dict
+    data = {field.name: getattr(request, field.name) for field in request.all_fields()}
+    # del data['websafeConferenceKey']
+
+    # add default values for those missing (both data model & outbound Message)
+    for df in DEFAULTS:
+        if data[df] in (None, []):
+            data[df] = DEFAULTS[df]
+            setattr(request, df, DEFAULTS[df])
+    # convert dates from strings to Date objects; set month based on start_date
+    if data['date']:
+        data['date'] = datetime.strptime(data['date'][:10], "%Y-%m-%d").date()
+        data['month'] = data['date'].month
+    else:
+        data['month'] = 0
+
+
+    # generate Session Key based on user ID and Conference name
+    # ID based on Profile key get Conference key from ID
+    c_key = ndb.Key(Conference, conf.key.id())
+    s_id = Session.allocate_ids(size=1, parent=c_key)[0]
+    s_key = ndb.Key(Session, s_key, parent=c_key)
+
+    data['key'] = c_key
+    data['organizerUserId'] = request.organizerUserId = user_id
+
+    Session(**data).put()
+
+    # check to see if speaker exists in other sections, add to memcache if so
+
+    return ""
+
+def _copySessionToForm(self, sesh):
+    """Copy relevant fields from Session to SessionForm."""
+    sf = SessionForm()
+    for field in sf.all_fields():
+        if hasattr(sesh, field.name):
+            # convert Time and Date to date string; just copy others
+            if field.name in ['startTime', 'date']:
+                setattr(sf, field.name, str(getattr(sesh, field.name)))
+            else:
+                setattr(sf, field.name, getattr(sesh, field.name))
+        elif field.name == "websafeKey":
+            setattr(cf, field.name, conf.key.urlsafe())
+    sf.check_initialized()
+    return sf
+
+
+@endpoints.method(SessionForm, SessionForm, path='sessions',
+        http_method='POST', name='createSession')
+def createSession(self, request):
+    """Create new Session in Conference."""
+    return self._createSession(request)
+
+
+
+
+@endpoints.method(SESSION_GET_REQUEST, SessionForms, path='session/{websafeConferenceKey}',
+        http_method='GET', name='getConferenceSessions')
+def getConferenceSessions(self, request):
+    """Return requested sessions (by websafeConferenceKey)."""
+    # make a query object for a specific ancestor
+    seshs = Session.query(ancestor=ndb.Key(Conference, websafeConferenceKey)
+
+    # return SessionForms
+    return SessionForms(
+        items=[self._copySessionToForm(seshs, getattr(prof, 'displayName'))
+
+
+    data = {field.name: getattr(request, field.name) for field in request.all_fields()}
+    # get Conference object from request; bail if not found
+    conf = ndb.Key(urlsafe=request.websafeConferenceKey).get()
+    if not conf:
+        raise endpoints.NotFoundException(
+            'No conference found with key: %s' % request.websafeConferenceKey)
+    
+    sesh = [(ndb.Key(Profile, conf.organizerUserId)) for session in conferences]
+    prof = ndb.get_multi(organisers)
+
+    prof = conf.key.parent().get()
+
+
+
+
+
+@endpoints.method(SESSION_GET_REQUEST, SessionForms, path='sessions/{websafeConferenceKey}/{typeOfSession}',
+        http_method='GET', name='getConferenceSessionsByType')
 def getConferenceSessionsByType(websafeConferenceKey, typeOfSession):
- Given a conference, return all sessions of a specified type (eg lecture, keynote, workshop)
+"""Given a conference, return all sessions of a specified type (eg lecture, keynote, workshop)"""
 
+    q = Session.query()
+    return q.filter(Session.typeOfSession == typeOfSession)
+
+    return SessionForms(items=[self._copySessionToForm(sesh, "")])
+
+
+
+
+
+
+@endpoints.method(SPEAKER_GET_REQUEST, SessionForms, path='sessions/{speaker}',
+        http_method='GET', name='getSessionsBySpeaker')
 def getSessionsBySpeaker(speaker):
- Given a speaker, return all sessions given by this particular speaker, across all conferences
-
-def createSession(SessionForm, websafeConferenceKey):
- -- open only to the organizer of the conference
-
-
+    #filter by property, returns all sessions by a particular speaker, across all conferences
+    q = Session.query()
+    return q.filter(Session.speaker == speaker)
 
 
 
 # Explain in a couple of paragraphs your design choices for session and speaker implementation.
-
-
+didn't create speaker as separate object, kept as a property (or create user as new attendee/user 
+bc helps with headcount and ensures all people in central store. e.g. if you need comprehensive user list for administrative purposes)
 
 
 
