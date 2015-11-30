7c6698a0ec454155a07834818b95443d8c20a3fb
mleaf <mariesleaf@gmail.com>
2015-11-30 12:07:34 -0500

    add session and sessionform models

--- P4_conference/FINAL_models.py
+++ P4_conference/FINAL_models.py
@@ -1,27 +1,53 @@
 # final app model additions
-
-
-# Ideally, create the session as a child of the conference.
-Sessions can have speakers, start time, duration, type of session (workshop, lecture etc…), location. You will need to define the Session class and the SessionForm class, as well as appropriate Endpoints.
+# The Session class is created as a child of the conference.
+Sessions can have speakers, start time, duration, type of session (workshop, lecture etc…), 
+location. You will need to define the Session class and the SessionForm class, as well as appropriate Endpoints.
 You are free to choose how you want to define speakers, eg just as a string or as a full fledged entity.
 
 
-class Session(Conference or ndb.Model):
+class Session(ndb.Model):
     """Session -- Session object"""
     sessionName     = ndb.StringField(required=True)
     highlights    	= ndb.StringField(repeated=True)
-    speaker			= ndb.StringField()
+    speaker			= ndb.StringField(required=True)
     duration        = ndb.IntegerField()
     typeOfSession   = ndb.StringField() # use enum?
     date       		= ndb.IntegerField()
-    startTime       = ndb.IntegerField() # in 24 hr notation so it can be ordered
+    startTime       = ndb.TimeProperty() # in 24 hr notation so it can be ordered
 
 class SessionForm(messages.Message):
     """SessionForm -- Session outbound form message"""
-    sessionName     = messages.StringField(1)
-    highlights    	= messages.StringField(2)
-    speaker			= messages.StringField(3)
-    duration        = messages.StringField(4, repeated=True)
-    typeOfSession   = messages.StringField(5) # use enum?
-    date       		= messages.IntegerField(6)
-    startTime       = messages.IntegerField(7) # in 24 hr notation so it can be ordered
+    sessionName          = messages.StringField(1)
+    highlights    	     = messages.StringField(2)
+    speaker			     = messages.StringField(3)
+    duration             = messages.StringField(4)
+    typeOfSession        = messages.StringField(5, repeated=True) # use enum?
+    date       		     = messages.StringField(6) # DateTimeField()
+    startTime            = messages.StringField(7) # DateTimeField()
+    websafeSessionKey = messages.StringField(8)
+    organizerDisplayName = messages.StringField(9)
+
+
+class SessionQueryForm(messages.Message):
+    """SessionQueryForm -- Session query inbound form message"""
+    field = messages.StringField(1)
+    operator = messages.StringField(2)
+    value = messages.StringField(3)
+
+class SessionQueryForms(messages.Message):
+    """ConferenceQueryForms -- multiple SessionQueryForm inbound form message"""
+    filters = messages.MessageField(SessionQueryForm, 1, repeated=True)
+
+
+
+
+class Wishlist(ndb.Model):
+    userID = ndb.StringProperty(required=True)
+    c_key  = ndb.KeyProperty(kind='Conference', required=True)
+    s_keys = ndb.KeyProperty(kind='Session', repeated=True)
+
+class WishlistForm(messages.Message):
+    userID = messages.StringField(1)
+    websafeSessionKey = messages.StringField(2)
+    websafeKey = messages.StringField(3)
+
